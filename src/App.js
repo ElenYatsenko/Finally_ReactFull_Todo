97e896f1742d3201b99267e43b06293bbc66da8e
Elena <15el.shev@gmail.com>
2022-12-14 22:26:43 +0200

    Todo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import MainLayout from "./layouts/MainLayout";
-import NotFound from "./components/NotFound";
-import Todos from "./components/Todos";
-import Home from "./components/Home";
-import Courses from "./components/Courses";
-import SingleCourse from "./components/SingleCourse";
 import "./App.css";
+import Todos from "./components/Todos/index.js";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contacts" element={<Contact />} />
-            <Route path="todo" element={<Todos />} />
-            <Route path="courses" element={<Courses />} />
-            <Route path="courses/:slug" element={<SingleCourse />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Todos />
     </div>
   );
 }
